9350b3cd64a906c6910aa2ef681030c43277203e
faheemkoduvali <52704760+faheemkoduvali@users.noreply.github.com>
2020-06-14 16:10:54 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+
+# Create your views here.
+
+def login(request):
+    if(request.method == 'POST'):
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request,"invalid credentials")
+            return redirect("login")
+
+
+    else:
+        return render(request,'login.html')
+
+def register(request):
+
+    if request.method == 'POST':
+        first_name=request.POST['firstname']
+        last_name=request.POST['lastname']
+        username=request.POST['username']
+        password1=request.POST['password']
+        password2=request.POST['password1']
+        email=request.POST['email']
+        if password1 == password2:
+            if User.objects.filter(username=username):
+                messages.info(request,'Username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email):
+                messages.info(request,'email taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('user created')
+                return login('login')
+        else:
+            messages.info(request,'Password is not matching')
+            return redirect('register')
+        return redirect('/')
+    else:
+        return render(request,'register.html ')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
